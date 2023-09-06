cb7ab38876f06bde35808b7c79d8dd5b5fa98129
João Pedro <joao.pedro.franchini.4@gmai.com>
2023-09-06 13:16:30 -0300

    Configurei o tailwind e o nuxt

---
+++ app.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+   <NuxtLayout>
+    <header>
+      <ul>
+        <li>
+          <NuxtLink to="/">Home</NuxtLink>
+          <NuxtLink to="/teste">Teste</NuxtLink>
+
+        </li>
+      </ul>
+    </header>
+    <NuxtPage />
+   </NuxtLayout>
+  </div>
+</template>
