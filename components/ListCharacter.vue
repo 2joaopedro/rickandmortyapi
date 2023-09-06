33e0e79af74c2f2d7f29e9648942198ace680933
João Pedro <joao.pedro.franchini.4@gmai.com>
2023-09-06 15:08:17 -0300

    Fiz a requsição da url character e trouxe os dados na tela

---
+++ components/ListCharacter.vue
@@ -0,0 +1,15 @@
+<script setup>
+ const character = defineProps(["name", "status", "species"])
+</script>
+<template>
+   <div>
+    <div></div>
+    <div>
+        <div>
+            <h1>{{ character.name }}</h1>
+            <p>{{ character.status }}</p>
+            <p>{{ character.species }}</p>
+        </div>
+    </div>
+   </div>
+</template>
--- pages/Character/index.vue
+++ pages/Character/index.vue
@@ -1,3 +1,25 @@
+<script setup>
+ 
+ let characters = reactive(ref())
+
+ onMounted(() => {
+    fetch("https://rickandmortyapi.com/api/character")
+    .then(response => response.json())
+    .then(response =>  {
+        characters.value = response.results
+        console.log(response)
+    })
+ })
+</script>
+
 <template>
-    <div  className="lex mx-40 my-5">Character page</div>
+    <main  className="lex mx-40 my-5">
+        <ListCharacter 
+            v-for="character in characters" 
+            :key="character.id" 
+            :name="character.name" 
+            :status="character.status" 
+            :species="character.species">
+        </ListCharacter>
+    </main>
 </template>
